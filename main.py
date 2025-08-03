3e3fb2f4750c736f7329ebe75b5c96ce4693a542
俞佳乐 <160045531+lifuqin258@users.noreply.github.com>
2025-08-03 17:28:20 +0800

    Update and rename main_alex.py to main.py

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
-import torchvision.models as models
-import torch
-import torch.nn as nn
-from Encoder import *
-from Decoder_alex_no import *
-from Aggregation import *
+import   进口 torchvision.models as   作为 models
+import   进口 torch   进口火炬
+import   进口 torch.nn    进口火炬。Nn as   作为 Nnas nn
+from Encoder import   进口 *
+from Decoder_alex_no import   进口 *
+from Aggregation import   进口 *
 
 
-class FM(nn.Module):
+class   类 FM   调频(nn.Module   模块):
     def __init__(self, num_classes=100):
-        super(FM, self).__init__()
-        alexnet = models.alexnet(pretrained=True)
+        super   超级(FM, self).__init__()
+        alexnet = models.alexnet(pretrained=True   真正的)
         alexnet.eval()
         self.feature_extractor_x = alexnet.features
         self.feature_extractor_y = alexnet.features
 
-        self.decoder = Decoder(dim=256)
+        self.decoder = Decoder   译码器(dim=256)
 
         self.aggregation = Aggregation()
 
         self.focal_x = FocalM(dim=256)
         self.focal_y = FocalM(dim=256)
-        self.dropout = nn.Dropout(p=0.6)
-        self.fc = nn.Linear(256 * 6 * 6, num_classes)
+        self.dropout = nn.Dropout   辍学(p=0.6)
+        self.fc = nn.Linear   线性(256 * 6 * 6, num_classes)
 
-    def forward(self, x, y):
+    def forward   向前(self, x, y):
         x = self.feature_extractor_x(x)
         y = self.feature_extractor_y(y)
 
